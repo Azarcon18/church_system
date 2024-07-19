58cc8d938c1b1a8b8b1c9f66df77f54df21d5759
Jagdon304 <jagdonjohncarlo@gmail.com>
2024-07-19 21:52:05 +0800

    commit

---
+++ pages/sadsad.php
@@ -0,0 +1,22 @@
+<?php require_once('../config.php'); ?>
+<!DOCTYPE HTML>
+<head>
+<title> sadsad | <?php echo $_settings->info('short_name') ?></title>
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta name="robots" content="index, follow">
+<meta property="og:type" content="article" />
+<meta property="og:title" content="sadsad" />
+<meta property="og:description" content="" />
+<meta property="og:image" content="http://localhost/church_management/dist/img/no-image-available.png" />
+<meta property="og:url" content="http://localhost/church_management/pages/sadsad.php" />
+<?php require_once('../inc/page_header.php') ?>
+</head>
+<?php include(base_app.'inc/body_block.php') ?>
+<h2>sadsad</h2>
+<input name="blog_id" value="8f14e45fceea167a5a36dedd4bea2543" type="hidden">
+<hr>
+<small><span class='text-muted'><i class='fa fa-calendar-day'></i> Published: Jun 15,2024 07:02 PM</span></small>
+<center><img src="http://localhost/church_management/dist/img/no-image-available.png" class="img-thumbnail img-banner" alt="http://localhost/church_management/pages/sadsad.php" /></center>
+
+<?php include(base_app.'inc/body_block_end.php') ?>
