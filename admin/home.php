daf43dd5197c132434a61bc1196a0ff9cdb0cbf0
Jagdon304 <jagdonjohncarlo@gmail.com>
2024-08-08 10:07:03 +0800

    commit

--- admin/home.php
+++ admin/home.php
@@ -21,18 +21,13 @@
           </div>
 
           <div class="col-12 col-sm-6 col-md-3">
-    <div class="info-box bg-color">
-        <span class="info-box-icon bg-primary elevation-1"><i class="fas fa-certificate"></i></span>
+            <div class="info-box bg-color">
+              <span class="info-box-icon bg-primary elevation-1"><i class="fas fa-certificate""></i></span>
 
-        <div class="info-box-content">
-            <span class="info-box-text">Death Certificate</span>
-            <span class="info-box-number text-right">
-                <!-- Your PHP code or additional content here -->
-            </span>
-        </div>
-    </div>
-</div>
-
+              <div class="info-box-content">
+                <span class="info-box-text">Death Certificate</span>
+                <span class="info-box-number text-right">
+                <?php 
     $appointment = $conn->query("SELECT count(id) as total FROM appointment_request WHERE status = 1")->fetch_assoc()['total'];
     echo number_format($appointment);
 ?>
